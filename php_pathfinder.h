d590f1a6eb32c988715d40ee304e7c57fe8fec2f
ahnsunggwan45 <51251620+ahnsunggwan45@users.noreply.github.com>
2026-05-05 03:17:16 +0900

    fix: guard extern "C" in php_pathfinder.h with __cplusplus
    PHP includes our header from internal_functions_cli.c (a C file) which
    chokes on unconditional extern "C". Add the standard __cplusplus guard
    so the header is valid in both C and C++ translation units.

--- php_pathfinder.h
+++ php_pathfinder.h
@@ -5,18 +5,24 @@
 #include "config.h"
 #endif
 
+#ifdef __cplusplus
 extern "C" {
+#endif
+
 #include "php.h"
-}
 
 extern zend_module_entry pathfinder_module_entry;
 #define phpext_pathfinder_ptr &pathfinder_module_entry
 
-#define PHP_PATHFINDER_VERSION  "0.1.0"
+#define PHP_PATHFINDER_VERSION  "0.1.1"
 #define PHP_PATHFINDER_EXTNAME  "pathfinder"
 
 #if defined(ZTS) && defined(COMPILE_DL_PATHFINDER)
 ZEND_TSRMLS_CACHE_EXTERN()
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif /* PHP_PATHFINDER_H */
